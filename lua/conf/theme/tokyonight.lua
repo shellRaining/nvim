4a5e2d735b263577b16b7c990c9bbd7e9bca259a
shell-Raining <shellRaining@gmail.com>
2023-03-09 00:15:13 +0800

    update view

---
+++ lua/conf/theme/tokyonight.lua
@@ -0,0 +1,25 @@
+-- https://github.com/askfiy/visual_studio_code
+
+
+local M = {
+    requires = {
+        "tokyonight",
+    },
+}
+
+function M.before() end
+
+function M.load()
+    M.tokyonight.setup({
+        transparent = true, -- Enable this to disable setting the background color
+        styles = {
+            sidebars = "transparent", -- style for sidebars, see below
+            floats = "transparent", -- style for floating windows
+        },
+    })
+    vim.cmd([[colorscheme tokyonight]])
+end
+
+function M.after() end
+
+return M
