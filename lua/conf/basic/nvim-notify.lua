6bd0b98e8c91ab4bbfc56d56442b6ae91388eb3c
shellRaining <shellRaining@gmail.com>
2023-04-08 18:27:15 +0800

    fix the fuck notify error msg

--- lua/conf/basic/nvim-notify.lua
+++ lua/conf/basic/nvim-notify.lua
@@ -11,6 +11,21 @@ local M = {
     },
 }
 
+local ignore_message = {
+    -- LSP
+    "%[LSP%]%[.+%] timeout",
+    "LSP%[id=%d*%] client has shut down after sending Log",
+    "LSP%[id=%d*%] client has shut down during progress update",
+    "LSP%[id=%d*%] client has shut down while creating progress report",
+    "LSP%[%d*%] client has shut down after sending a workspace/configuration request",
+    "method textDocument/signatureHelp is not supported by any of the servers registered for the current buffer",
+    "method textDocument/documentSymbol is not supported by any of the servers registered for the current buffer",
+    "warning: multiple different client offset_encodings detected for buffer, this is not supported yet",
+    'Server "emmet_ls" does not support documentSymbols.',
+    'No information available',
+    "Failed to attach to volar for current buffer. Already attached to volar",
+}
+
 function M.before()
     M.register_key()
 end
@@ -19,7 +34,7 @@ function M.load()
     M.notify.setup({
         stages = "fade",
         timeout = 3000,
-        fps = 24,
+        fps = 60,
         icons = {
             ERROR = icons.Error,
             WARN = icons.Warn,
@@ -33,6 +48,11 @@ function M.after()
     vim.notify = setmetatable({}, {
         ---@diagnostic disable-next-line: unused-local
         __call = function(self, message, ...)
+            for _, v in ipairs(ignore_message) do
+                if message:match(v) then
+                    return
+                end
+            end
             return M.notify(message, ...)
         end,
         __index = M.notify,
