92229cc3d504a6e858551cfdde6a4c71ae1af4b6
shellRaining <shellRaining@gmail.com>
2023-09-07 22:38:20 +0800

    update mapping

--- lua/conf/tools/code_runner.lua
+++ lua/conf/tools/code_runner.lua
@@ -29,7 +29,33 @@ function M.after()
 end
 
 function M.register_key()
+    local function close_all_terminals()
+        -- 获取当前窗口的缓冲区ID
+        local current_buffer = vim.api.nvim_get_current_buf()
+
+        -- 遍历所有窗口
+        local windows = vim.api.nvim_list_wins()
+        for _, win in ipairs(windows) do
+            vim.api.nvim_set_current_win(win)
+            local buffer_type = vim.api.nvim_buf_get_option(0, "buftype")
+            if buffer_type == "terminal" then
+                vim.cmd("close")
+            end
+        end
+
+        -- 切换回原来的窗口
+        vim.api.nvim_set_current_win(vim.fn.win_getid(current_buffer))
+    end
+
+    vim.api.nvim_set_keymap("n", "<Leader>ct", "<Cmd>call close_all_terminals()<CR>", { noremap = true, silent = true })
     api.map.bulk_register({
+        {
+            mode = { "n" },
+            lhs = "<leader>tc",
+            rhs = close_all_terminals,
+            options = { silent = true },
+            description = "close all terminal",
+        },
         {
             mode = { "n" },
             lhs = "<leader>rf",
