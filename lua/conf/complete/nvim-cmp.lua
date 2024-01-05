0958488e29fd283b1b491898195f461bdae9547a
shellRaining <shellRaining@gmail.com>
2024-01-05 19:24:22 +0800

    update cmp

--- lua/conf/complete/nvim-cmp.lua
+++ lua/conf/complete/nvim-cmp.lua
@@ -21,43 +21,14 @@ local M = {
     event = { "InsertEnter", "CmdlineEnter" },
 }
 
-function M.before()
-    -- complete the floating window settings of the menu
-    M.complete_window_settings = {
-        fixed = false,
-        min_width = 15,
-        max_width = 15,
-    }
-    -- whether to allow the following completion sources to have the same keywords as other completion sources
-    M.duplicate_keywords = {
-        -- allow duplicate keywords
-        ["luasnip"] = 1,
-        -- do not allow duplicate keywords
-        ["nvim_lsp"] = 0,
-        ["buffer"] = 0,
-        ["path"] = 0,
-        ["cmdline"] = 0,
-    }
-end
-
 function M.load()
-    -- send cmp to aid_cmp module
     aid_nvim_cmp.receive_cmp(M.cmp, M.copilot_suggestion)
 
     M.cmp.setup({
-        view = {
-            -- "custom", "wildmenu" or "native"
-            entries = "custom",
-        },
-        experimental = {
-            ghost_text = true,
-        },
-        -- uncheck auto-select (gopls)
-        preselect = M.cmp_types.cmp.PreselectMode.None,
+        view = { entries = "custom" },
+        experimental = { ghost_text = { hl_group = "CmpGhostText" } },
         -- Insert or Replace
-        confirmation = {
-            default_behavior = M.cmp.ConfirmBehavior.Insert,
-        },
+        confirmation = { default_behavior = M.cmp.ConfirmBehavior.Insert },
         snippet = {
             expand = function(args)
                 M.luasnip.lsp_expand(args.body)
@@ -70,12 +41,6 @@ function M.load()
             { name = "path" },
             { name = "buffer" },
         }),
-        -- define buttons
-        -- • i: valid in insert mode
-        -- • s: valid in select mode
-        -- • c: valid in command mode
-        -- if you don't want to auto-complete when selecting, then fill in the following content in the item selection
-        -- { behavior = M.cmp.SelectBehavior.Select }
         mapping = {
             ["<c-p>"] = aid_nvim_cmp.select_prev_item(),
             ["<c-n>"] = aid_nvim_cmp.select_next_item(),
@@ -88,61 +53,19 @@ function M.load()
             ["<c-d>"] = aid_nvim_cmp.select_next_n_item(5),
             ["<c-k>"] = aid_nvim_cmp.toggle_complete_menu(),
         },
-        -- define sorting rules
-        sorting = {
-            priority_weight = 2,
-            comparators = {
-                M.cmp.config.compare.offset,
-                M.cmp.config.compare.exact,
-                -- M.cmp.config.compare.scopes,
-                M.cmp.config.compare.score,
-                M.cmp.config.compare.recently_used,
-                M.cmp.config.compare.locality,
-                M.cmp.config.compare.kind,
-                -- M.cmp.config.compare.sort_text,
-                M.cmp.config.compare.length,
-                M.cmp.config.compare.order,
-                -- aid_nvim_cmp.under_compare,
-                -- aid_nvim_cmp.source_compare,
-                -- aid_nvim_cmp.kind_compare,
-            },
-        },
-        -- define the style of menu completion options
         formatting = {
             -- sort menu
             fields = { "kind", "abbr", "menu" },
             format = function(entry, vim_item)
-                local abbr = vim_item.abbr
-                local kind = vim_item.kind
-                local source = entry.source.name
+                local kind = "<" .. vim_item.kind .. ">"
+                local source = "(" .. entry.source.name .. ")"
 
-                -- vim_item.kind = ("%s %s"):format(icons[kind], kind)
-                -- vim_item.menu = ("<%s>"):format(source:upper())
-
-                -- icon_prefix
-                vim_item.kind = (" %s "):format(icons[kind])
-                vim_item.menu = ("<%s>"):format(kind)
-
-                vim_item.dup = M.duplicate_keywords[source] or 0
-
-                -- determine if it is a fixed window size
-                if M.complete_window_settings.fixed and vim.fn.mode() == "i" then
-                    local min_width = M.complete_window_settings.min_width
-                    local max_width = M.complete_window_settings.max_width
-                    local truncated_abbr = vim.fn.strcharpart(abbr, 0, max_width)
-
-                    if truncated_abbr ~= abbr then
-                        vim_item.abbr = ("%s %s"):format(truncated_abbr, "…")
-                    elseif abbr:len() < min_width then
-                        local padding = (" "):rep(min_width - abbr:len())
-                        vim_item.abbr = ("%s %s"):format(abbr, padding)
-                    end
-                end
+                vim_item.kind = (" %s "):format(icons[vim_item.kind])
+                vim_item.menu = ("%-10s %s"):format(kind, source:upper())
 
                 return vim_item
             end,
         },
-        -- define the appearance of the completion menu
         window = not options.float_border and {}
             or {
                 completion = M.cmp.config.window.bordered({
