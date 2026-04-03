993ab8b0bd12775da5ec464934cdb2ff4e8cd147
shellRaining <shellRaining@gmail.com>
2026-04-03 22:56:30 +0800

    feat: migrate nvim-treesitter from master to main branch for Neovim 0.12 compatibility

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,60 +1,43 @@
+local ensure_installed = {
+  "bash",
+  "c",
+  "css",
+  "html",
+  "javascript",
+  "jsdoc",
+  "json",
+  "lua",
+  "luadoc",
+  "luap",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "query",
+  "regex",
+  "tsx",
+  "typescript",
+  "vim",
+  "vue",
+  "vimdoc",
+  "yaml",
+}
+
 local treesitter = {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
-  branch = "master",
+  branch = "main",
   build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
   config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "css",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "vue",
-        "vimdoc",
-        "yaml",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = function(_, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = false,
-      },
-      -- incremental selection
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<cr>",
-          node_incremental = "<cr>",
-          node_decremental = "<bs>",
-          scope_incremental = false,
-        },
-      },
+    require("nvim-treesitter").setup()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function(ev)
+        local lang = vim.treesitter.language.get_lang(vim.bo[ev.buf].filetype)
+        if not lang then
+          return
+        end
+        pcall(vim.treesitter.start, ev.buf, lang)
+      end,
     })
   end,
 }
@@ -64,9 +47,9 @@ local autotag = {
   event = { "BufReadPost", "BufNewFile", "BufWritePre" },
   opts = {
     opts = {
-      enable_close = true, -- Auto close tags
-      enable_rename = true, -- Auto rename pairs of tags
-      enable_close_on_slash = true, -- Auto close on trailing </
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = true,
     },
     aliases = {
       ["wxml"] = "html",
@@ -82,7 +65,23 @@ local autopair = {
 
 local comment = { "numToStr/Comment.nvim", opts = {} }
 
-local commentstring = { "JoosepAlviste/nvim-ts-context-commentstring", event = "VeryLazy", opts = {} }
+local commentstring = {
+  "JoosepAlviste/nvim-ts-context-commentstring",
+  event = "VeryLazy",
+  opts = {},
+  config = function(_, opts)
+    require("ts_context_commentstring").setup(opts)
+    local utils = require("ts_context_commentstring.utils")
+    local orig_is_active = utils.is_treesitter_active
+    utils.is_treesitter_active = function(bufnr)
+      local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+      if not ok or parser == nil then
+        return false
+      end
+      return orig_is_active(bufnr)
+    end
+  end,
+}
 
 return {
   treesitter,
