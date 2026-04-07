d91d24eeb871586e7b2ae0e89d01dc17a9f544de
shellRaining <shellRaining@gmail.com>
2026-04-07 11:25:25 +0800

    feat: add oxfmt detect files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,7 +6,7 @@ local frontend_formatter = function()
     return { "dprint" }
   elseif root(0, { ".prettierrc", ".prettierrc.json", ".prettierrc.js", "prettier.config.js" }) then
     return { "prettier", stop_after_first = true }
-  elseif root(0, { ".oxfmtrc.jsonc" }) then
+  elseif root(0, { ".oxfmtrc.json", ".oxfmtrc.jsonc", "oxfmt.config.ts" }) then
     return { "oxfmt" }
   elseif root(0, { "eslint.config.js", "eslint.config.mjs" }) then
     return {}
